2b39ae97cff738c6c23d5721a0a8673a99c7f3da
KhaBaoDuyen <143988268+KhaBaoDuyen@users.noreply.github.com>
2025-09-22 22:49:44 +0700

    [ fix ] - Heading

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,28 +1,25 @@
 export function Heading({ text, left = true, right = true }) {
   return (
-    <span className="flex w-full  gap-2 items-center">
+    <span className="flex w-full items-center gap-2 justify-center">
       {left && (
         <img
           src="https://cdn.prod.website-files.com/67f70667bbc9554c154f545d/67f70667bbc9554c154f54bb_arrow-left.svg"
-          loading="lazy"
           alt="arrow-left"
-          className="badge_left w-full lg:w-[12vw]"
+          className="badge_left w-[30px] lg:w-[12vw] max-w-[120px]"
         />
       )}
 
       <p
         className="
-          text-white 
-          whitespace-nowrap 
-          px-6 py-3 
-          rounded-full 
+          text-white whitespace-nowrap 
+          px-6 py-3 rounded-full 
           flex justify-center items-center
-          bg-gradient-to-b from-[#0a0a0a4b] to-[#0a0a0a4b] 
+          bg-gradient-to-b from-[#0a0a0a4b] to-[#0a0a0a4b]
           shadow-[inset_0_4px_8px_rgba(255,255,255,0.1),0_2px_10px_rgba(0,0,0,0.5)]
           border border-white/10
           transition-all duration-300 
           hover:shadow-[inset_0_6px_12px_rgba(255,255,255,0.2),0_4px_20px_rgba(0,0,0,0.7)]
-            "
+        "
       >
         {text}
       </p>
@@ -30,9 +27,8 @@ export function Heading({ text, left = true, right = true }) {
       {right && (
         <img
           src="https://cdn.prod.website-files.com/67f70667bbc9554c154f545d/67f70667bbc9554c154f54bd_arrow-right.svg"
-          loading="lazy"
           alt="arrow-right"
-          className="badge_right w-full lg:w-[12vw]"
+          className="badge_right w-[30px] lg:w-[12vw] max-w-[120px]"
         />
       )}
     </span>
