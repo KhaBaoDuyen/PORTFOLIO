a17e0db097c4c116b9e05d5323604eb898c24eb3
KhaBaoDuyen <143988268+KhaBaoDuyen@users.noreply.github.com>
2025-12-31 17:07:24 +0700

    git update footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,10 +2,10 @@ import { FaGithub, FaLinkedin, FaEnvelope, FaPhoneAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 export default function Footer() {
   return (
-    <footer className="w-full h-20 flex flex-col md:flex-row justify-between items-center border-t border-gray-200/30 mt-20 py-6">
+    <footer className="w-full h-20 flex flex-col justify-between items-center border-t border-gray-200/30 mt-20 py-6">
        <div className="flex gap-4">
         <Link
-          to="https://github.com/your-username"
+          to="https://github.com/KhaBaoDuyen"
           target="_blank"
           rel="noopener noreferrer"
           className="p-2 border border-white rounded-full text-white transition hover:bg-white hover:text-black hover:border-transparent"
@@ -13,7 +13,7 @@ export default function Footer() {
           <FaGithub className="h-5 w-5" />
         </Link>
         <Link
-          to="https://www.linkedin.com/in/your-profile"
+          to="www.linkedin.com/in/bao-duyen-kha-thi-41622736a"
           target="_blank"
           rel="noopener noreferrer"
           className="p-2 border border-white rounded-full text-white transition hover:bg-white hover:text-black hover:border-transparent"
@@ -21,13 +21,13 @@ export default function Footer() {
           <FaLinkedin className="h-5 w-5" />
         </Link>
         <Link
-          to="duyenktb1410@example.com"
+          to="duyenktb1410@gmail.com"
           className="p-2 border border-white rounded-full text-white transition hover:bg-white hover:text-black hover:border-transparent"
         >
           <FaEnvelope className="h-5 w-5" />
         </Link>
         <Link
-          to="https://zalo.me/your-zalo-id"
+          to="https://zalo.me/0337019197"
           target="_blank"
           rel="noopener noreferrer"
           className="p-2 border border-white rounded-full text-white transition hover:bg-white hover:text-black hover:border-transparent"
